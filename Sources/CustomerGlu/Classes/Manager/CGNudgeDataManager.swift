aafecdd7538e4b03e106573d52ec49df75a8fc05
Ankit Jain <ankitjain0211@gmail.com>
2023-06-14 00:03:59 +0530

    June Release | Added logs

--- Sources/CustomerGlu/Classes/Manager/CGNudgeDataManager.swift
+++ Sources/CustomerGlu/Classes/Manager/CGNudgeDataManager.swift
@@ -16,34 +16,58 @@ class CGNudgeDataManager: NSObject {
     
     private override init() {
         super.init()
+        if CustomerGlu.isDebugingEnabled {
+            print("** Init CGNudgeDataManager **")
+        }
         let _ = self.getCacheNudgeDataModelsArray()
     }
     
     func getCacheNudgeDataModelsArray() -> [CGNudgeDataModel] {
+        if CustomerGlu.isDebugingEnabled {
+            print("** CGNudgeDataManager :: Get Nudge Data Array **")
+        }
         cacheNudgeDataModelsArray = getDataFromUserDefaults()
         return cacheNudgeDataModelsArray
     }
 
     func saveNudgeData(with model: CGNudgeDataModel) {
+        if CustomerGlu.isDebugingEnabled {
+            print("** CGNudgeDataManager :: Save Nudge Data **")
+        }
         cacheNudgeDataModelsArray.append(model)
         saveDataToUserDefaults()
     }
     
     func deleteNudgeData(with model: CGNudgeDataModel) {
+        if CustomerGlu.isDebugingEnabled {
+            print("** CGNudgeDataManager :: Start Delete Nudge Data **")
+        }
+        
         if let firstIndex = cacheNudgeDataModelsArray.firstIndex(where: { data in
             data.nudgeId == model.nudgeId
         }) {
+            if CustomerGlu.isDebugingEnabled {
+                print("** CGNudgeDataManager :: End Delete Nudge Data **")
+            }
             cacheNudgeDataModelsArray.remove(at: firstIndex)
             saveDataToUserDefaults()
         }
     }
     
     private func saveDataToUserDefaults() {
+        if CustomerGlu.isDebugingEnabled {
+            print("** CGNudgeDataManager :: Save to User Defaults **")
+        }
         let data = convertDataToDict()
         userDefault.set(data, forKey: udKey)
+        printInfo()
     }
     
     private func getDataFromUserDefaults() -> [CGNudgeDataModel] {
+        if CustomerGlu.isDebugingEnabled {
+            print("** CGNudgeDataManager :: Get from User Defaults **")
+        }
+        
         if let cacheData = userDefault.object(forKey: udKey) as? [[AnyHashable: Any]] {
             var data: [CGNudgeDataModel] = []
             for dict in cacheData {
@@ -63,11 +87,12 @@ class CGNudgeDataManager: NSObject {
         return data
     }
     
-    private func updateCacheNudgeDataModelsArray() -> [[AnyHashable: Any]] {
-        var data: [[AnyHashable: Any]] = []
-        for model in cacheNudgeDataModelsArray {
-            data.append(model.convertToDict())
+    private func printInfo() {
+        if CustomerGlu.isDebugingEnabled {
+            print("** CGNudgeDataManager :: All Saved Nudge ID's **")
+            for (index, model) in cacheNudgeDataModelsArray.enumerated() {
+                print("** CGNudgeDataManager :: Index : \(index), Nudge ID : \(model.nudgeId ?? "") **")
+            }
         }
-        return data
     }
 }
