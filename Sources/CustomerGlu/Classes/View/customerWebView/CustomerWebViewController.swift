cc8cecca402a1b207c6702c33b7dfa5b4d953612
devyasir2023 <137758939+devyasir2023@users.noreply.github.com>
2023-09-12 18:30:43 +0530

    dismissing sheet on failure

--- Sources/CustomerGlu/Classes/View/customerWebView/CustomerWebViewController.swift
+++ Sources/CustomerGlu/Classes/View/customerWebView/CustomerWebViewController.swift
@@ -254,8 +254,6 @@ public class CustomerWebViewController: UIViewController, WKNavigationDelegate, 
             self.canopencgwebview = false
             self.openCGWebView()
         }
-        self.isSSLChecked = false
-        self.isSSLSuccess = false
     }
     
     func loadwebView(url: String, x: CGFloat, y: CGFloat) {
@@ -311,24 +309,11 @@ public class CustomerWebViewController: UIViewController, WKNavigationDelegate, 
         CGEventsDiagnosticsHelper.shared.sendDiagnosticsReport(eventName: CGDiagnosticConstants.CG_DIAGNOSTICS_WEBVIEW_START_PROVISIONAL, eventType:CGDiagnosticConstants.CG_TYPE_DIAGNOSTICS, eventMeta: [:])
     }
 
-    private var isSSLChecked: Bool = false
-    private var isSSLSuccess: Bool = false
     public func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
         guard #available(iOS 12.0, *) else { return }
         
         guard let serverTrust = challenge.protectionSpace.serverTrust,
               let certificate = SecTrustGetCertificateAtIndex(serverTrust, 0) else {
-            return
-        }
-        
-        guard isSSLChecked == false else {
-            DispatchQueue.global(qos: .background).async {
-                if self.isSSLSuccess {
-                    completionHandler(.useCredential, URLCredential(trust: serverTrust))
-                } else {
-                    completionHandler(.cancelAuthenticationChallenge, nil)
-                }
-            }
             return
         }
         
@@ -341,16 +326,14 @@ public class CustomerWebViewController: UIViewController, WKNavigationDelegate, 
             guard let localCertificateData: NSData = ApplicationManager.getLocalCertificateAsNSData() else {
                 return
             }
-            self.isSSLChecked = true
             if isServerTrusted && remoteCertificateData.isEqual(to: localCertificateData as Data) {
                 print("Certificate matched")
-                self.isSSLSuccess = true
                 completionHandler(.useCredential, URLCredential(trust: serverTrust))
                 return
             } else {
                 print("Certificate does not matched")
-                self.isSSLSuccess = false
                 completionHandler(.cancelAuthenticationChallenge, nil)
+                self.dismiss(animated: true)
                 return
             }
         }
