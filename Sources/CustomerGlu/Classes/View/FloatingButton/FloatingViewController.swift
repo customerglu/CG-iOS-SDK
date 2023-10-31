2b562fe288bf4b3f7f82c50ca7f83f346ed0dfe6
Kausthubh Sukhesha Adhikari <kausthubhadhikari@gmail.com>
2023-10-31 10:47:36 +0530

    October Release | Added Dispatch Queue in Floating button

--- Sources/CustomerGlu/Classes/View/FloatingButton/FloatingViewController.swift
+++ Sources/CustomerGlu/Classes/View/FloatingButton/FloatingViewController.swift
@@ -294,9 +294,11 @@ extension UIView {
 
 extension UIWindow {
     func dismiss() {
-        isHidden = true
-        if #available(iOS 13, *) {
-            windowScene = nil
+        DispatchQueue.main.async{
+            self.isHidden = true
+            if #available(iOS 13, *) {
+                self.windowScene = nil
+            }
         }
     }
 }
